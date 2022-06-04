1d6a3b12c6d0f25d426e7b64e92ee401dd4c61b5
kalifun <kalifun@163.com>
2022-06-04 19:15:14 +0800

    #6 ✨AccountMovieWatchlist

--- lib/src/instances/account.dart
+++ lib/src/instances/account.dart
@@ -524,3 +524,36 @@ class AccountRatedTVEpisodesResults {
     return data;
   }
 }
+
+class AccountMovieWatchlist {
+  int? page;
+  List<AccountFavoriteMoviesResults>? results;
+  int? totalPages;
+  int? totalResults;
+
+  AccountMovieWatchlist(
+      {this.page, this.results, this.totalPages, this.totalResults});
+
+  AccountMovieWatchlist.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <AccountFavoriteMoviesResults>[];
+      json['results'].forEach((v) {
+        results!.add(new AccountFavoriteMoviesResults.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['page'] = this.page;
+    if (this.results != null) {
+      data['results'] = this.results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = this.totalPages;
+    data['total_results'] = this.totalResults;
+    return data;
+  }
+}
