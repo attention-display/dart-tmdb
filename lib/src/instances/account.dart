40993f8b04b1a2a2fa4de8fbb93d07fb447282ed
kalifun <kalifun@163.com>
2022-06-04 18:47:46 +0800

    #6 ✨AccountFavoriteMovies

--- lib/src/instances/account.dart
+++ lib/src/instances/account.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 class AccountDetails {
   Avatar? avatar;
   int? id;
@@ -80,7 +82,7 @@ class Gravatar {
 
 class AccountCreatedLists {
   int? page;
-  List<Results>? results;
+  List<AccountCreatedListsResults>? results;
   int? totalPages;
   int? totalResults;
 
@@ -90,9 +92,9 @@ class AccountCreatedLists {
   AccountCreatedLists.fromJson(Map<String, dynamic> json) {
     page = json['page'];
     if (json['results'] != null) {
-      results = <Results>[];
+      results = <AccountCreatedListsResults>[];
       json['results'].forEach((v) {
-        results!.add(new Results.fromJson(v));
+        results!.add(new AccountCreatedListsResults.fromJson(v));
       });
     }
     totalPages = json['total_pages'];
@@ -111,7 +113,7 @@ class AccountCreatedLists {
   }
 }
 
-class Results {
+class AccountCreatedListsResults {
   String? description;
   int? favoriteCount;
   int? id;
@@ -121,7 +123,7 @@ class Results {
   String? name;
   String? posterPath;
 
-  Results(
+  AccountCreatedListsResults(
       {this.description,
       this.favoriteCount,
       this.id,
@@ -131,7 +133,7 @@ class Results {
       this.name,
       this.posterPath});
 
-  Results.fromJson(Map<String, dynamic> json) {
+  AccountCreatedListsResults.fromJson(Map<String, dynamic> json) {
     description = json['description'];
     favoriteCount = json['favorite_count'];
     id = json['id'];
@@ -152,6 +154,115 @@ class Results {
     data['list_type'] = this.listType;
     data['name'] = this.name;
     data['poster_path'] = this.posterPath;
+    return data;
+  }
+}
+
+class AccountFavoriteMovies {
+  int? page;
+  List<AccountFavoriteMoviesResults>? results;
+  int? totalPages;
+  int? totalResults;
+
+  AccountFavoriteMovies(
+      {this.page, this.results, this.totalPages, this.totalResults});
+
+  AccountFavoriteMovies.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <AccountFavoriteMoviesResults>[];
+      json['results'].forEach((v) {
+        results!.add(new AccountFavoriteMoviesResults.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['page'] = this.page;
+    if (this.results != null) {
+      data['results'] = this.results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = this.totalPages;
+    data['total_results'] = this.totalResults;
+    return data;
+  }
+}
+
+class AccountFavoriteMoviesResults {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  String? releaseDate;
+  String? posterPath;
+  int? popularity;
+  String? title;
+  bool? video;
+  int? voteAverage;
+  int? voteCount;
+
+  AccountFavoriteMoviesResults(
+      {this.adult,
+      this.backdropPath,
+      this.genreIds,
+      this.id,
+      this.originalLanguage,
+      this.originalTitle,
+      this.overview,
+      this.releaseDate,
+      this.posterPath,
+      this.popularity,
+      this.title,
+      this.video,
+      this.voteAverage,
+      this.voteCount});
+
+  AccountFavoriteMoviesResults.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    if (json['genre_ids'] != null) {
+      genreIds = <int>[];
+      json['genre_ids'].forEach((v) {
+        genreIds!.add(v);
+      });
+    }
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    releaseDate = json['release_date'];
+    posterPath = json['poster_path'];
+    popularity = json['popularity'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['adult'] = this.adult;
+    data['backdrop_path'] = this.backdropPath;
+    if (this.genreIds != null) {
+      data['genre_ids'] = this.genreIds!.map((v) => v).toList();
+    }
+    data['id'] = this.id;
+    data['original_language'] = this.originalLanguage;
+    data['original_title'] = this.originalTitle;
+    data['overview'] = this.overview;
+    data['release_date'] = this.releaseDate;
+    data['poster_path'] = this.posterPath;
+    data['popularity'] = this.popularity;
+    data['title'] = this.title;
+    data['video'] = this.video;
+    data['vote_average'] = this.voteAverage;
+    data['vote_count'] = this.voteCount;
     return data;
   }
 }
